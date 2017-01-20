f5c250da80f64fbd9afd6570a7bc7f4e10d86168
FaySie <sel12342001@gmail.com>
2017-01-20 18:20:48 +0800

    增加scroll down button

--- modules/mod_fay/tmpl/default.php
+++ modules/mod_fay/tmpl/default.php
@@ -15,27 +15,34 @@ $doc->addStyleSheet('templates/fay/css/index.css');
 ?>
 <div class="content">
     <div class="section section1" id="section1">
-        <img class="img-responsive" src="<?php echo $logo; ?>" alt="logo" />
+    	<img class="img-responsive" src="<?php echo $logo; ?>" alt="logo" />
+	    	<a class="scroll" href="#section2">
+	    		<span class='mouse-container'>
+					<span class='mouse'>
+			    		<span class='scroll-down'></span>
+	  				</span>
+				</span>
+	    	</a>
     </div>
     <div class="section section2" id="section2">
         <div>
             <h1><?php echo $section2_title; ?></h1>
             <h4><?php echo $section2_desc ?></h4>
-            <a href="<?php echo $section2_link ?>"><?php echo $section2_link_desc ?></a>
+            <a class="btn_more" href="<?php echo $section2_link ?>"><?php echo $section2_link_desc ?></a>
         </div>
     </div>
     <div class="section section3" id="section3">
         <div>
             <h1><?php echo $section3_title ?></h1>
             <h4><?php echo $section3_desc ?></h4>
-            <a href="<?php echo $section3_link ?>"><?php echo $section3_link_desc ?></a>
+            <a class="btn_more" href="<?php echo $section3_link ?>"><?php echo $section3_link_desc ?></a>
         </div>
     </div>
     <div class="section section4" id="section4">
         <div>
             <h1><?php echo $section4_title ?></h1>
             <h4><?php echo $section4_desc ?></h4>
-            <a href="<?php echo $section4_link ?>"><?php echo $section4_link_desc ?></a>
+            <a class="btn_more" href="<?php echo $section4_link ?>"><?php echo $section4_link_desc ?></a>
         </div>
     </div>
     <div class="section section5" id="section5">
