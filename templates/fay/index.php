6613d35c85d395756cc435088a89cbdd73de85e5
FaySie <sel12342001@gmail.com>
2017-01-26 15:35:38 +0800

    修正電腦版文章子分類跑版問題 & 手機版主選單異常

--- templates/fay/index.php
+++ templates/fay/index.php
@@ -6,6 +6,7 @@ $doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/
 $doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/css/nav.css');
 $doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/css/slidshow.css');
 
+$doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/style.js');
 $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/slidshow.js');
 $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/bootstrap.js');
 $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/smooth-scroll.js');
