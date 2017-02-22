8b417af6e602d2541b4830bd75f28e7d95cb64fb
FaySie <sel12342001@gmail.com>
2017-02-22 19:34:11 +0800

    修正載入頁面 & 手機版分類選單bug

--- templates/fay/js/style.js
+++ templates/fay/js/style.js
@@ -5,23 +5,13 @@
 		$(window).load(function(){
         	$('#loading').fadeOut();
         	$(".container").removeClass("loading_scroll");
+			$('html, body').animate({
+				scrollTop : 0
+			});
         });
 
 		$(".btn_cat").click(function(){
 			$(".menu").slideToggle("slow");
 		});
-
-		$(".menu_bg").click(function(){
-			if($(this).hasClass("clicked")){
-				$(".menu").css("display", "none");
-				$(".btn_cat").css("display", "block");
-				$(this).removeClass("clicked");
-			} else {
-				$(".menu").css("display", "block");
-				$(".btn_cat").css("display", "none");
-				$(this).addClass("collapsed clicked");
-			}
-		});
 	});
 })(jQuery)
-
