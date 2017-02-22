a6f43cdcc2fab750a7bedfd536a7bc8a6b30aa30
FaySie <sel12342001@gmail.com>
2017-02-22 19:46:09 +0800

    修正手機版分類選單及主選單顯示異常

--- templates/fay/js/style.js
+++ templates/fay/js/style.js
@@ -2,16 +2,33 @@
 	$(document).ready(function() {
 		$(".container").addClass("loading_scroll");
 
+		// loading頁面
 		$(window).load(function(){
         	$('#loading').fadeOut();
         	$(".container").removeClass("loading_scroll");
+
+        	// 結束後，將頁面強制回到頂端
 			$('html, body').animate({
 				scrollTop : 0
 			});
         });
 
+		// 手機版分類選單toggle效果
 		$(".btn_cat").click(function(){
 			$(".menu").slideToggle("slow");
+		});
+
+		// 修正手機版分類選單顯示異常
+		$(".menu_bg").click(function(){
+			if($(this).hasClass("clicked")){
+				$(".menu").css("display", "none");
+				$(".btn_cat").css("display", "block");
+				$(this).removeClass("clicked");
+			} else {
+				$(".menu").css("display", "block");
+				$(".btn_cat").css("display", "none");
+				$(this).addClass("collapsed clicked");
+			}
 		});
 	});
 })(jQuery)
